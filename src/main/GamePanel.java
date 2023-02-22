3ac9828a815076fde3c023786e44e3aae2bc6efd
ThanhBT12 <105102395+kjjjoP3@users.noreply.github.com>
2023-02-22 21:25:04 +0700

    add pause program

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -32,7 +32,7 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	
 	TileManager tileM = new TileManager(this);
-	KeyHandler keyH = new KeyHandler();
+	KeyHandler keyH = new KeyHandler(this);
 	
 	Sound music = new Sound();
 	Sound se = new Sound();
@@ -46,6 +46,11 @@ public class GamePanel extends JPanel implements Runnable {
 	//Entity and Object
 	public Player player = new Player(this, keyH);
 	public SuperObject obj[] = new SuperObject[10];
+	
+	// game state
+	public int gameState;
+	public final int playState = 1;
+	public final int pauseState = 2;
 	
 	//set player's default posotion
 	
@@ -61,8 +66,9 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	public void setupGame() {
 		aSetter.setObject();
-		
 		playMusic(0);
+		stopMusic();
+		gameState = playState;
 	}
 
 	
@@ -108,7 +114,13 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 	
 	public void update() {
-		player.update();
+		
+		if(gameState == playState) {
+			player.update();
+		}
+		if(gameState == pauseState) {
+			// nothing
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -6,9 +6,14 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener{
 
 	public boolean upPressed, downPressed, leftPressed, rightPressed;
-	
+	GamePanel gp;
 	//debug
 	boolean checkDrawTime = false;
+	
+	public KeyHandler(GamePanel gp) {
+		this.gp = gp;
+	}
+	
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
@@ -29,6 +34,13 @@ public class KeyHandler implements KeyListener{
 		}
 		if(code == KeyEvent.VK_D) {
 			rightPressed = true;
+		}
+		if(code == KeyEvent.VK_P) {
+			if(gp.gameState == gp.playState) {
+				gp.gameState = gp.pauseState;
+			}else if (gp.gameState == gp.pauseState) {
+				gp.gameState = gp.playState;
+			}
 		}
 		
 		//Debug
